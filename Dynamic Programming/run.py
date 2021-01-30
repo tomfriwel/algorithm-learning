882f782b103f8aced05cdcf8b7df71d719a67551
tomfriwel <976932447@qq.com>
2021-01-30 15:31:12 +0800

    m

--- Dynamic Programming/run.py
+++ Dynamic Programming/run.py
@@ -1,4 +1,17 @@
+
+
+calc_count = 0
+
+
 def fib(N):
+    global calc_count
+    calc_count = calc_count + 1
     if N == 1 or N == 2:
         return 1
     return fib(N - 1) + fib(N - 2)
+
+
+print(fib(20))  # 6765
+print("calc count: {0}".format(calc_count))
+
+memo = {}
